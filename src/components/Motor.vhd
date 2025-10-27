35d8dc0b5e846baeed415489cc58f90c9c327a7b
Luisa <luisaferreirass08@gmail.com>
2025-10-27 14:38:19 -0300

    feat: implementing engine logic

--- src/components/Motor.vhd
+++ src/components/Motor.vhd
@@ -10,6 +10,10 @@ entity Motor is
         comando    : in  std_logic_vector(1 downto 0);  
         -- 00 = parado, 01 = subir, 10 = descer
 
+        -- Entrada porta
+        porta : in std_logic
+        -- 1 = aberta, 0 = fechada
+
         -- Saída de sensor (movimento atual)
         em_movimento : out std_logic;  -- 1 = movendo, 0 = parado
         direcao      : out std_logic_vector(1 downto 0)  -- mesma codificação do comando
@@ -18,5 +22,28 @@ end entity;
 
 architecture Behavioral of Motor is
 begin
-    -- A lógica interna será a máquina de estados que você vai criar depois
+    process(clk, rst)
+    begin
+        if rst = '1' then
+        em_movimento <= '0';
+        direcao <= '00';
+        
+        elsif rising_edge(clk) then
+            if porta = '0' then -- porta fechada
+                if comando = '01' then -- subir
+                    em_movimento <= '1';
+                    direcao <= '01';
+                elsif comando = '10' then -- descer
+                    em_movimento <= '1';
+                    direcao <= '10';
+                else -- parado
+                    em_movimento <= '0';
+                    direcao <= '00';
+                end if;
+            else -- porta aberta
+                em_movimento <= '0';
+                direcao <= '00';
+            end if;
+        end if;
+    end process;
 end architecture;
